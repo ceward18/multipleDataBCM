bbd31a487e4a750876434b29b466f9ac40c2b9ae
ceward18 <ward-c@umn.edu>
2024-08-23 13:16:02 -0500

    run data analysis longer

--- data_analysis/submitFull.sh
+++ data_analysis/submitFull.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=32G
 #SBATCH --array=1-36
 #SBATCH --output=./out/Array.%A_%a.out
