93e3f4618bf73e7a2c378eaec1156a0f94fc409b
Dr. Caitlin Ward <ward-c@ahl04.agate.msi.umn.edu>
2026-03-06 09:08:48 -0600

    updates from msi

--- supplemental_simulations/submitSIHRD.sh
+++ supplemental_simulations/submitSIHRD.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --time=36:00:00
 #SBATCH --mem=64000M
-#SBATCH --array=1-90
+#SBATCH --array=11,13,14,17-19,38,41-46,50,71,73-80
 #SBATCH --output=./out/Array.%A_%a.out
 #SBATCH --error=./err/Array.%A_%a.error
 
